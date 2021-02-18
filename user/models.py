ef5e0bbb8969cfe7e8ecee3b32509385472b81d3
Yeo-SoMang <77438122+Yeo-SoMang@users.noreply.github.com>
2021-02-18 15:57:51 +0900

    Update models.py

--- user/models.py
+++ user/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=20, primary_key=True)
-    email = models.EmailField(max_length=50)
+    username = models.CharField(max_length=20, primary_key=True, verbose_name='사용자명')
+    email = models.EmailField(max_length=50, verbose_name='비밀번호')
     password = models.CharField(max_length=20)
 
     def __str__(self):
         return self.username
+
+    class Meta:
+        db_table = 'users'
