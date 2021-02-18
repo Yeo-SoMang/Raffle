9dbb064da747957c39240256cbab328e2e97b5e0
Yeo-SoMang <77438122+Yeo-SoMang@users.noreply.github.com>
2021-02-18 15:58:07 +0900

    Update views.py

--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import FormView
 # 패스워드 암호화 해주는 함수.
 from django.contrib.auth.hashers import make_password
@@ -13,30 +13,16 @@ class JoinView(FormView):
     success_url = '/'
 
     def form_valid(self, form):
-        cleaned_data = form.cleaned_data #dict
+        cleaned_data = form.cleaned_data  # dict
         email = cleaned_data.get('email')
-        password = make_password(cleaned_data.get('password'))#암호화
+        password = make_password(cleaned_data.get('password'))  # 암호화
         username = cleaned_data.get('username')
         user = User(email=email, password=password, username=username)
         user.save()
 
         return super().form_valid(form)
 
-# 로그인
-# class LoginView(FormView):
-#     template_name = 'User/login.html'
-#     from_class = LoginForm
-#     success_url = '/'
-#
-#     def form_valid(self, form):
-#         cleaned_data = form.cleaned_data
-#         username = cleaned_data.get('username')
-#         # db에 저장되어 있는 email
-#         # Session객체-dictionary 형태:key-value쌍이 필요
-#         # 로그인 -> session에 로그인 여부를 확인할 수 있는 체크값을 넣어둔다.
-#         self.request.session['user'] = username
-#
-#         return super().form_valid(form)
+
 # 로그인 처리 View
 class LoginView(FormView):
     template_name = 'User/login.html'
@@ -49,7 +35,7 @@ class LoginView(FormView):
         # db에 저장되어 있는 email
         # Session객체-dictionary 형태:key-value쌍이 필요
         # 로그인 -> session에 로그인 여부를 확인할 수 있는 체크값을 넣어둔다.
-        self.request.session['user'] = username
+        self.request.session['User'] = username
         # request: HttpRequest(사용자 HTTP 요청정보)
         return super().form_valid(form)
 
@@ -58,4 +44,4 @@ class LoginView(FormView):
 def logout(request):
     request.session.clear()
 
-    return render(request, 'main/index.html')
+    return redirect('/')
